5721d09114e2ac1350c6c5ab0e7d1bf5db9e9583
Chu Chun Yin, Jeremy <129842660+84ds84d8s@users.noreply.github.com>
2025-12-15 19:16:30 +0800

    Change dir to limeage instead of cm

--- lineage.mk
+++ lineage.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/samsung/lt01wifi/lt01wifi.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lt01wifi
